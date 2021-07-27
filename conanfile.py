6e623d51ac1b5d38ae6a93d0c0384b32010644d3
Anderson Florence <aflorence@xenotran.com>
2021-07-27 13:14:41 -0400

    added conanfile package recipe

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, MSBuild
+import os
+
+def get_version():
+    # return os.environ.get('GitVersion_SemVer', None)
+    return "0.1.0-version.h.1"
+
+class ConanPackageInfo(ConanFile):
+    name = "AddressBook"
+    version = get_version()
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "visual_studio"
+    exports_sources = "include/*", "src/*", "make/*", "test/*"
+    requires = "boost/1.75.0" # comma-separated list of requirements
+    #    default_options = {"boost:shared": True}
+
+    def build(self):
+        msbuild = MSBuild(self)
+        msbuild.build("make/msvc/AddressBook.sln", upgrade_project=False, use_env=False, verbosity="normal")
+        
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.exe", dst="bin", src="make/msvc/x64/Release")
+        self.copy("*.lib", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["AddressBookLib"]
